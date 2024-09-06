642754d19b2aa1ed6050548f4b5b98eac5d294bf
Catherine Crenshaw <55335367+ccrenshawcodes@users.noreply.github.com>
2024-09-05 19:35:16 -0600

    Add Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,22 @@
-function Card( {img, title} ) {
+import { useState } from 'react';
+
+function Card( {img, title, endGame, adjustScore} ) {
+  const [hasBeenClicked, setHasBeenClicked] = useState(false);
+
+  function handleClick () {
+    if (hasBeenClicked) {
+      endGame();
+    } else {
+      setHasBeenClicked(true);
+      adjustScore();
+    }
+  }
 
   return (
-    <div className="memory-card">
+    <div 
+      className="memory-card"
+      onClick={handleClick}
+    >
       <img src={img}></img>
       <h2>{title}</h2>
     </div>
