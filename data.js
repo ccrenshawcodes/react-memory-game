2c9cce1b0770c3e56c2dd3444dfc34c7f4f22eee
Catherine Crenshaw <55335367+ccrenshawcodes@users.noreply.github.com>
2024-09-09 16:57:08 -0600

    Change back to web URLs for smooth deploy

--- data.js
+++ data.js
@@ -1,62 +1,62 @@
 export const data = [
-{
-    id: 0,
-    catName: 'The Castle of Cagliostro',
-    photo: 'src/assets/Castle_of_Cagliostro_poster.png',
-  },
-{
-    id: 1,
-    catName: 'Nausicaä of the Valley of the Wind',
-    photo: 'src/assets/Nausicaaposter.jpg',
-  },
-{
-    id: 2,
-    catName: 'Castle in the Sky',
-    photo: 'src/assets/Castle_in_the_Sky_(1986).png',
-  },
-{
-    id: 3,
-    catName: 'My Neighbor Totoro',
-    photo: 'src/assets/My_Neighbor_Totoro_-_Tonari_no_Totoro_(Movie_Poster).jpg',
-  },
-{
-    id: 4,
-    catName: 'Kiki\'s Delivery Service',
-    photo: 'src/assets/Kiki\'s_Delivery_Service_(Movie).jpg',
-  },
-{
-    id: 5,
-    catName: 'Porco Rosso',
-    photo: 'src/assets/Porco_Rosso_(Movie_Poster).jpg',
-  },
-{
-    id: 6,
-    catName: 'Princess Mononoke',
-    photo: 'src/assets/Princess_Mononoke_Japanese_poster.png',
-  },
-{
-    id: 7,
-    catName: 'Spirited Away',
-    photo: 'src/assets/Spirited_Away_Japanese_poster.png',
-  },
-{
-    id: 8,
-    catName: 'Howl\'s Moving Castle',
-    photo: 'src/assets/Howls-moving-castleposter.jpg',
-  },
-{
-    id: 9,
-    catName: 'Ponyo',
-    photo: 'src/assets/Ponyo.png',
-  },
-{
-    id: 10,
-    catName: 'The Wind Rises',
-    photo: 'src/assets/Kaze_Tachinu_poster.jpg',
-  },
-{
-    id: 11,
-    catName: 'The Boy and the Heron',
-    photo: 'src/assets/How_Do_You_Live_poster.jpg',
-  },
-]
+  {
+      id: 0,
+      catName: 'The Castle of Cagliostro',
+      photo: 'https://upload.wikimedia.org/wikipedia/en/4/4f/Castle_of_Cagliostro_poster.png',
+    },
+  {
+      id: 1,
+      catName: 'Nausicaä of the Valley of the Wind',
+      photo: 'https://upload.wikimedia.org/wikipedia/en/b/bc/Nausicaaposter.jpg',
+    },
+  {
+      id: 2,
+      catName: 'Castle in the Sky',
+      photo: 'https://upload.wikimedia.org/wikipedia/en/f/f5/Castle_in_the_Sky_%281986%29.png',
+    },
+  {
+      id: 3,
+      catName: 'My Neighbor Totoro',
+      photo: 'https://upload.wikimedia.org/wikipedia/en/0/02/My_Neighbor_Totoro_-_Tonari_no_Totoro_%28Movie_Poster%29.jpg',
+    },
+  {
+      id: 4,
+      catName: 'Kiki\'s Delivery Service',
+      photo: 'https://upload.wikimedia.org/wikipedia/en/0/07/Kiki%27s_Delivery_Service_%28Movie%29.jpg',
+    },
+  {
+      id: 5,
+      catName: 'Porco Rosso',
+      photo: 'https://upload.wikimedia.org/wikipedia/en/f/fc/Porco_Rosso_%28Movie_Poster%29.jpg',
+    },
+  {
+      id: 6,
+      catName: 'Princess Mononoke',
+      photo: 'https://upload.wikimedia.org/wikipedia/en/8/8c/Princess_Mononoke_Japanese_poster.png',
+    },
+  {
+      id: 7,
+      catName: 'Spirited Away',
+      photo: 'https://upload.wikimedia.org/wikipedia/en/d/db/Spirited_Away_Japanese_poster.png',
+    },
+  {
+      id: 8,
+      catName: 'Howl\'s Moving Castle',
+      photo: 'https://upload.wikimedia.org/wikipedia/en/a/a0/Howls-moving-castleposter.jpg',
+    },
+  {
+      id: 9,
+      catName: 'Ponyo',
+      photo: 'https://upload.wikimedia.org/wikipedia/en/5/51/Ponyo.png',
+    },
+  {
+      id: 10,
+      catName: 'The Wind Rises',
+      photo: 'https://upload.wikimedia.org/wikipedia/en/a/a3/Kaze_Tachinu_poster.jpg',
+    },
+  {
+      id: 11,
+      catName: 'The Boy and the Heron',
+      photo: 'https://upload.wikimedia.org/wikipedia/en/4/41/How_Do_You_Live_poster.jpg',
+    },
+  ]
